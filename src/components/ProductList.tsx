322a041fed16d447d6404c1dafb02562e5b130bd
simon1pierre <simonpierreturamyumukiza@gmail.com>
2025-09-28 11:18:54 -0700

    sunday mornig commits

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 import Categories from './Categories';
 import ProductCard from './ProductCard';
 import Link from 'next/link';
+import Filter from './Filter';
+
+
+
+
+
  const products: ProductTypes = [
   {
     id: 1,
@@ -113,10 +119,11 @@ import Link from 'next/link';
     images: { blue: "/products/8b.png", green: "/products/8gr.png" },
   },
 ];
-const ProductList = ({category}:{category:string}) => {
+const ProductList = ({category, params}:{category:string, params:"homepage" | "products"}) => {
   return (
     <div className="w-full">
       <Categories/>
+      {params === "products" && <Filter />}
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
          {products.map(product=>(
           <ProductCard key={product.id} product={product}></ProductCard>
